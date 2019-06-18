2d2234ff31f22f766ceee636aa2ef2d848a2e776
Lucky <lakatos.martin@gmail.com>
2019-06-19 00:47:37 +0200

    TEST SCRIPT: test if nginx web page is up

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,22 @@
 import os
+import urllib2
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+# def test_hosts_file(host):
+#     f = host.file('/etc/hosts')
 
+#     assert f.exists
+#     assert f.user == 'root'
+#     assert f.group == 'root'
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_index_page(host):
+    link = "http://127.0.0.1"
+    try:
+        urllib2.urlopen(link)
+    except urllib2.HTTPError, e:
+        print(e.code)
+    except urllib2.URLError, e:
+        print(e.reason)
